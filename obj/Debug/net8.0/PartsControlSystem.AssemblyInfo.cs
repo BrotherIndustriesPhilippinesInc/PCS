bbad966d90c3cd5011102edbdeed0ae4f77bc4fa
John Carlo Asi <asijo@brothergroup.net>
2026-04-15 09:16:54 +0800

    Added Transaction Logs Module

--- obj/Debug/net8.0/PartsControlSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/PartsControlSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PartsControlSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd1682178773139ee676572a329a257c1986236d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fed9dd61e11f440730a0f26836597bb976d64512")]
 [assembly: System.Reflection.AssemblyProductAttribute("PartsControlSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PartsControlSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
